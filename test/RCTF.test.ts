8ee46c454f524f00253a0da157d24822bb2ae7f4
Shefer Arkadii <arkadiy.shefer@gmail.com>
2024-08-01 22:11:52 +0300

    RCTF tests

--- test/RCTF.test.ts
+++ test/RCTF.test.ts
@@ -1,20 +1,60 @@
 import { loadFixture, ethers, expect } from "./setup";
-//import type { RCTF } from "../typechain-types";
+import type { RCTF } from "../typechain-types";
 
 describe("RCTF", function () {
   async function deploy() {
-    const [user1, user2, user3] = await ethers.getSigners();
+    const [owner, receiver, user] = await ethers.getSigners();
 
-    const Factory = await ethers.getContractFactory("RCTF", user1);
-    const rctf = await Factory.deploy();
+    const Factory = await ethers.getContractFactory("RCTF", owner);
+    const rctf: RCTF = await Factory.deploy({ value: ethers.parseUnits("2", "ether") });
     await rctf.waitForDeployment();
 
-    return { user1, user2, user3, rctf }
+    return { owner, receiver, user, rctf }
   }
 
-  it("Deploy test", async function () {
-    const { user1, user2, user3, rctf } = await loadFixture(deploy);
+  it("Should allow to receive and send payments", async function () {
+    const { owner, receiver, user, rctf } = await loadFixture(deploy);
 
+    const amount = ethers.parseUnits("2", "ether");
+    const nonce = 1;
 
+    const hash = ethers.solidityPackedKeccak256(["address", "uint256", "uint256", "address"], [receiver.address, amount, nonce, await rctf.getAddress()]);
+
+    const messageHashBin = ethers.toBeArray(hash);
+    const signature = await owner.signMessage(messageHashBin);
+
+    const tx = await rctf.connect(receiver).claim(amount, nonce, signature);
+
+    expect(tx).to.changeEtherBalance(receiver, amount);
+  });
+
+  it("Should not allow to receive fake amount", async function () {
+    const { owner, receiver, user, rctf } = await loadFixture(deploy);
+
+    const amount = ethers.parseUnits("2", "ether");
+    const nonce = 1;
+
+    const hash = ethers.solidityPackedKeccak256(["address", "uint256", "uint256", "address"], [receiver.address, amount, nonce, await rctf.getAddress()]);
+
+    const messageHashBin = ethers.toBeArray(hash);
+    const signature = await owner.signMessage(messageHashBin);
+
+    const moreThanSignedAmount = ethers.parseUnits("3", "ether");
+    await expect(rctf.connect(receiver).claim(moreThanSignedAmount, nonce, signature)).to.be.revertedWith("Invalid signature!")
+  });
+
+  it("Should not allow to receive amount more than 1 time", async function () {
+    const { owner, receiver, user, rctf } = await loadFixture(deploy);
+
+    const amount = ethers.parseUnits("2", "ether");
+    const nonce = 1;
+
+    const hash = ethers.solidityPackedKeccak256(["address", "uint256", "uint256", "address"], [receiver.address, amount, nonce, await rctf.getAddress()]);
+
+    const messageHashBin = ethers.toBeArray(hash);
+    const signature = await owner.signMessage(messageHashBin);
+
+    const tx = await rctf.connect(receiver).claim(amount, nonce, signature);
+    await expect(rctf.connect(receiver).claim(amount, nonce, signature)).to.be.revertedWith("Nonce alreade used!")
   });
 })
